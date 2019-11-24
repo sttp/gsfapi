0430e675ec19fbbcf65b09ff430b0b1af2374653
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-11-24 10:31:47 -0500

    Added sample test results and comparisons

---
+++ src/samples/InteropTest/Results/InteropTest.bat
@@ -0,0 +1,3 @@
+START "GSF-TEST" "C:\Projects\sttp\gsfapi\src\samples\InteropTest\bin\Debug\InteropTest.exe" localhost 7165
+TIMEOUT 4
+START "CPP-TEST" "C:\Projects\sttp\cppapi\build\output\x64\Debug\samples\InteropTest.exe" localhost 7165
