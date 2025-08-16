707563148a3391aa23b6f8cf25e89737e1ba0fa9
gsfbuildbot <support@gridprotectionalliance.org>
2025-08-16 01:27:35 -0400

    sttp.gsfapi: Version change for build v1.0.567.0-master.

--- src/lib/sttp.gsf/Properties/AssemblyInfo.cs
+++ src/lib/sttp.gsf/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.566.0")]
-[assembly: AssemblyVersion("1.0.566.0")]
-[assembly: AssemblyFileVersion("1.0.566.0")]
+// [assembly: AssemblyVersion("1.0.567.0")]
+[assembly: AssemblyVersion("1.0.567.0")]
+[assembly: AssemblyFileVersion("1.0.567.0")]
